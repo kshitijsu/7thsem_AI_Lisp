787a20261b5ded618f4ad87d3bdd0acc821dfe35
kshitijsu <k18.ukey@gmail.com>
2019-09-09 10:53:13 +0530

    Basics

--- basics.lisp
+++ basics.lisp
@@ -19,3 +19,35 @@
 ;Division first
 (write (+(* 60 (/ 9 5))32))
 ; Output -> 140
+
+(setq x 10)
+(print x)
+;10
+(print (type-of x))
+;(INTEGER 0 281474976710655) 
+
+(setq x 34.257)
+(print x)
+;34.257
+(print (type-of x))
+;SINGLE-FLOAT 
+
+(setq x 123.57)
+(print x)
+;123.57 
+
+(setq x NIL)
+(print x)
+;NIL 
+(print (type-of x))
+;NULL
+
+(setq x 62)
+(print x)
+;62 
+
+(setq x "Kshitij")
+(print x)
+;"Kshitij"
+(print (type-of x))
+;(SIMPLE-BASE-STRING 7)
