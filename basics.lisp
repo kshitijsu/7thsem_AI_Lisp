104bcd2a4fe50977d04d8e80b8c01d1418d78ca0
kshitijsu <k18.ukey@gmail.com>
2019-09-09 10:29:04 +0530

    Basics

--- basics.lisp
+++ basics.lisp
@@ -1,7 +1,21 @@
 ;gnu clisp 2.49
 
+
 (write-line "Add two numbers, inhabit evaluation ")
 (write (+ 2 5))
+
 (write-line " ")
+
 (write-line "Add two numbers, evaluate expression ")
 (write '(+ 2 5))
+
+
+;Evaluting the expression: (60 * 9 / 5 + 32)
+
+;Multiplication first
+(write (+(/ (* 60 9) 5)32))
+; Output -> 140
+
+;Division first
+(write (+(* 60 (/ 9 5))32))
+; Output -> 140
